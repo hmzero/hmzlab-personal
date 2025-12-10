ed85cf2e776c21b0127f37877ab0bd5cd5d6d2b9
Hunter Stone <hunter.m.stone@gmail.com>
2025-12-09 17:43:32 -0700

    Initial setup

--- src/settings.ts
+++ src/settings.ts
@@ -1,19 +1,19 @@
 export const profile = {
-	fullName: 'Your Name',
+	fullName: 'Hunter Stone',
 	title: '',
 	institute: '',
 	author_name: '', // Author name to be highlighted in the papers section
 	research_areas: [
-		// { title: 'Physics', description: 'Brief description of the research interest', field: 'physics' },
+		{ title: 'Physics', description: 'Brief description of the research interest', field: 'physics' },
 	],
 }
 
 // Set equal to an empty string to hide the icon that you don't want to display
 export const social = {
-	email: '',
-	linkedin: '',
-	x: 'https://www.x.com/',
-	github: '',
+	email: 'hstone@mines.edu',
+	linkedin: 'https://www.linkedin.com/in/hstone02/',
+	x: '',
+	github: 'https://github.com/hmzero',
 	gitlab: '',
 	scholar: '',
 	inspire: '',
@@ -22,18 +22,18 @@ export const social = {
 }
 
 export const template = {
-	website_url: 'https://localhost:4321', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
+	website_url: 'https://hmzlab.net', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
 	menu_left: false,
 	transitions: true,
-	lightTheme: 'light', // Select one of the Daisy UI Themes or create your own
-	darkTheme: 'dark', // Select one of the Daisy UI Themes or create your own
+	lightTheme: 'synthwave', // Select one of the Daisy UI Themes or create your own
+	darkTheme: 'dim', // Select one of the Daisy UI Themes or create your own
 	excerptLength: 200,
 	postPerPage: 5,
     base: '' // Repository name starting with /
 }
 
 export const seo = {
-	default_title: 'Astro Academia',
-	default_description: 'Astro Academia is a template for academic websites.',
+	default_title: 'Hunter Stone',
+	default_description: 'Hunter Stone\'s personal website.',
 	default_image: '/images/astro-academia.png',
 }
